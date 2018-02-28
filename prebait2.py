f6d70e350ce50871adce2a288f935a08299726b0
joseC <ignacio3437@gmail.com>
2018-02-28 16:18:53 +0800

    geneset2genome_blast OK

--- prebait2.py
+++ prebait2.py
@@ -24,19 +24,19 @@ def param2dict(param_path):
         param_dict = dict(zip(paramdict_keys, param_list))
     return param_dict
 
-def run_command(command,command_out_path=None):
+def run_command(command, command_out_path=None):
     """Runs command in subprocess. Prints error if commmand did not run. 
     if outpath given it will print output there, otherwise this function returns it. """
     command_output = subprocess.getstatusoutput(command)
+    if command_output[0]:
+        print(f"{command} Failed")
+        sys.exit(1)
     if command_out_path:
         with open(command_out_path, "w") as out_handle:
             out_handle.write(command_output[1])
-            if command_output[0]:
-                print(f"{command} Failed")
-                sys.exit(1)
     return command_output[1]
 
-def run_command_parallel(command,num_threads):
+def run_command_parallel(command, num_threads):
     p = Pool(num_threads)
     p.map(run_command, command)
     return 
@@ -44,7 +44,8 @@ def run_command_parallel(command,num_threads):
 def seqname_lister(seq_file):
     """Reads all of the names in a fasta file and returns a list"""
     records = SeqIO.parse(seq_file,"fasta")
-    return [r.id for r in records]
+    seqname_list = [r.id for r in records]
+    return seqname_list
 
 def DNAorAA(seq_file):
     """Reads a file and guesses if DNA or RNA.
@@ -62,7 +63,7 @@ def DNAorAA(seq_file):
         alphabet = 'dna'
     return alphabet
 
-def blaster(param_dict, query, blastdb_prefix,blast_out_path=None):
+def blaster(param_dict, query, blastdb_prefix, blast_out_path = None):
     """Runs a blast search and returns the names of the hit sequences from the blast database.
     If out_path specified saves results to file, otherwise returns as string"""
     alphabet = DNAorAA(query)
@@ -72,7 +73,7 @@ def blaster(param_dict, query, blastdb_prefix,blast_out_path=None):
         blast_prog = 'tblastn'
     threads = param_dict["Threads"]
     #create blast database if there is none
-    if not os.path.isfile(blastdb_prefix+".nin"):
+    if not Path(blastdb_prefix+".nin").exists():
         dbcommand = f"makeblastdb -in {blastdb_prefix} -parse_seqids -dbtype nucl"
         run_command(dbcommand)
     #Run Blast
@@ -80,38 +81,34 @@ def blaster(param_dict, query, blastdb_prefix,blast_out_path=None):
     blast_output = run_command(bcommand,command_out_path=blast_out_path)
     return blast_output
 
-def genomecdsparser(instring,outstring):
-    outstring = re.sub(r".*cds_(.*\..*)_.*",r"\1",instring)
-    return outstring
 
 param_path = '/Users/josec/Desktop/git_repos/PreBait/Example_paramfile.txt'
 param_dict = param2dict(param_path)
-print(param_dict['Threads'])
-
-name_duds=seqname_lister('/Users/josec/Desktop/NudiPreBait2/1_Generate_Target_Sets/AgalmaRuns/AgalmaCDSfast5/AgalmaOutput/Fast52genes.fa')[:10]
-##Saves output to file:
-# blaster(param_dict, '/Users/josec/Desktop/PyPreBait/Targets/Fast5_Chr_wes_dna_test.fa', param_dict["Genome"],'/Users/josec/Desktop/PyPreBait/Targets/Fast5_Chr_wes_dna_test.txt')
-##Does search and replace, then saves to file
-rawblast_output = blaster(param_dict, '/Users/josec/Desktop/PyPreBait/Targets/Fast5_Chr_wes_dna_test.fa', param_dict["Genome"])
-blast_output = re.sub(r".*cds_(.*\..*)_.*",r"\1",rawblast_output)
-with open('/Users/josec/Desktop/PyPreBait/Targets/Fast5_Chr_wes_dna_test.txt','w') as out_handle:
-    out_handle.write(blast_output)
+geneset = "/Users/josec/Desktop/PyPreBait/Targets/Fast5_Chr_wes_dna_test.fa"
+from pathlib import Path
 
 
+def geneset2genome_blast(param_dict,geneset):
+    """ Blasts a geneset to a genome cds like this one:
+    #http://mirrors.vbi.vt.edu/mirrors/ftp.ncbi.nih.gov/genomes/refseq/invertebrate/Aplysia_californica/latest_assembly_versions/GCF_000002075.1_AplCal3.0/
+    reformats the blast results to the refseq protein IDs and removes duplicate hits. The blast cutoff is e-20
+    """
+    rawblast_output = blaster(param_dict, query = geneset, blastdb_prefix = param_dict["Genome"])
+    blast_output = re.sub(r".*cds_(.*\..*)_.*",r"\1",rawblast_output)
+    unique_blast_lines = set(blast_output.split("\n"))
+    blast_outpath = Path(geneset).with_suffix('.txt')
+    with open(blast_outpath,'w') as out_handle:
+        out_handle.write("\n".join(unique_blast_lines))
+    return
 
-# class TargetSet:
-#     """Takes a fasta file of target seqs (like agalma output) and a reference genome.
-#     Blasts the targets to the genome and retrieves the names of the genome hits and the sequences """
-#     def __init__(self,target_file,param_dict):
-#         self.threads = param_dict["Threads"]
-#         self.target_file = target_file
-#         self.genome_file = param_dict["Genome"]
 
-#     def targetNamelist(self):
-#         self.target_file # fasta read file here
-#         name_list="FillTHISIN"
-#         return name_list
-    
-#     def genomeOrthologs(self):
-#         pass
+
+
+
+
+
+
+
+
+
 
